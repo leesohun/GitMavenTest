7a25da8c796749015d1c3e3ad15a3b98a33d716d
lhss <121652275+leesohun@users.noreply.github.com>
2023-06-02 12:40:43 +0900

    mul() 추가

--- src/main/java/com/example/demo/CalcController.java
+++ src/main/java/com/example/demo/CalcController.java
@@ -17,9 +17,9 @@ public class CalcController {
 		
 	}
 	
-	@GetMapping(value="/sub")
-	public String sub() {
-		return "25 - 36 = " + (25-36);
+	@GetMapping(value="/mul")
+	public String mul() {
+		return "25 * 36 = " + (25*36);
 		
 	}
 
